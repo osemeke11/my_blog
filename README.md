4a28212948e140d4a4d2e02ed1e723f8a16b2659
Osemeke Samuel <mrexcelsam1@live.com>
2017-01-08 00:14:38 +0100

    Abstract Sensitive Data into .env file using composer

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # My Blog
 
 My first stab at writing a blog in PHP
+
+### Deploy
+
+1. git clone https://github.com/osemeke11/my_blog.git my_blog
+2. cd my_blog
+3. nano .env
+4. DB_HOST=
+	DB_PASS=
+	DB_USER=
+	DB_NAME=
--- libraries/DB_details.php
+++ libraries/DB_details.php
@@ -1,7 +1,14 @@
 <?php
-	define('DB_HOST', "localhost");
-	define('DB_NAME', "my_blog");
-	define('DB_USER', "root");
-	define('DB_PASS', "church6468");
+
+	require __DIR__ .'/../vendor/autoload.php';
+
+	$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+	$dotenv->load();
+
+
+	define('DB_HOST', getenv("DB_HOST"));
+	define('DB_NAME', getenv("DB_NAME"));
+	define('DB_USER', getenv("DB_USER"));
+	define('DB_PASS', getenv("DB_PASS"));
 
 ?>
