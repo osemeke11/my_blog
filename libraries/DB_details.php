8a4732a204f0fad3d1200a10003ec0c9938cfe3b
Osemeke Samuel <mrexcelsam1@live.com>
2017-02-09 01:18:45 +0100

    Editing Insert_article.php and remove text editor

--- libraries/DB_details.php
+++ libraries/DB_details.php
@@ -1,11 +1,8 @@
 <?php
 
 	require __DIR__ .'/../vendor/autoload.php';
-
 	$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 	$dotenv->load();
-
-
 	define('DB_HOST', getenv("DB_HOST"));
 	define('DB_NAME', getenv("DB_NAME"));
 	define('DB_USER', getenv("DB_USER"));
